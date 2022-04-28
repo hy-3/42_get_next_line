db074594cf71718ed2ecda8cc6deb9a3b7b3f9c9
Hiroki Yamamoto <hymt73@gmail.com>
2022-04-28 12:53:14 +0200

    fixed codes to handle when malloc fails and also to align with 42 coding standards

--- get_next_line.c
+++ get_next_line.c
@@ -6,19 +6,55 @@
 /*   By: hiyamamo <hiyamamo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/23 16:37:35 by hiyamamo          #+#    #+#             */
-/*   Updated: 2022/04/26 19:38:10 by hiyamamo         ###   ########.fr       */
+/*   Updated: 2022/04/28 12:43:52 by hiyamamo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+int	fill_container(char **container, char **buff)
+{
+	char	*tmp_container;
+
+	if (*container == NULL)
+	{
+		*container = ft_strdup(*buff);
+		if (*container == NULL)
+		{
+			free(*buff);
+			return (0);
+		}
+	}
+	else
+	{
+		tmp_container = *container;
+		*container = ft_strjoin(*container, *buff);
+		if (*container == NULL)
+		{
+			free(tmp_container);
+			return (0);
+		}
+		free(tmp_container);
+	}
+	free(*buff);
+	return (1);
+}
+
 int	read_with_container(int fd, char **container)
 {
 	char	*buff;
-	char	*tmp_container;
 	int		bytes_read;
 
 	buff = (char *) malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (buff == NULL)
+	{
+		if (*container != NULL)
+		{
+			free(*container);
+			container = NULL;
+			return (0);
+		}
+	}
 	bytes_read = read(fd, buff, BUFFER_SIZE);
 	if (bytes_read == -1 || bytes_read == 0)
 	{
@@ -26,18 +62,18 @@ int	read_with_container(int fd, char **container)
 		return (0);
 	}
 	buff[bytes_read] = '\0';
-	if (*container == NULL)
-		*container = ft_strdup(buff);
-	else
-	{
-		tmp_container = *container;
-		*container = ft_strjoin(*container, buff);
-		free(tmp_container);
-	}
-	free(buff);
+	if (fill_container(container, &buff) == 0)
+		return (0);
 	if (bytes_read < BUFFER_SIZE)
 		return (0);
 	return (1);
+}
+
+char	*free_container(char **container)
+{
+	free(*container);
+	*container = NULL;
+	return (NULL);
 }
 
 char	*update_str_and_container(char *split_ptr, char **container, char *str)
@@ -46,16 +82,21 @@ char	*update_str_and_container(char *split_ptr, char **container, char *str)
 	char	*tmp_buff;
 
 	tmp_buff = ft_strdup(*container);
+	if (tmp_buff == NULL)
+		return (free_container(container));
 	tmp_buff[(split_ptr - *container) + 1] = '\0';
 	str = ft_strdup(tmp_buff);
 	free(tmp_buff);
 	tmp_container = *container;
 	*container = ft_strdup(split_ptr + 1);
-	if (**container == '\0')
+	if (*container == NULL)
 	{
-		free(*container);
-		*container = NULL;
+		free(tmp_container);
+		free(str);
+		return (NULL);
 	}
+	if (**container == '\0')
+		free_container(container);
 	free(tmp_container);
 	return (str);
 }
@@ -74,11 +115,13 @@ char	*get_next_line(int fd)
 	if (split_ptr != NULL)
 		str = update_str_and_container(split_ptr, &container, str);
 	else
+	{
 		if (container != NULL)
 		{
 			str = ft_strdup(container);
 			free(container);
 			container = NULL;
 		}
+	}
 	return (str);
 }
